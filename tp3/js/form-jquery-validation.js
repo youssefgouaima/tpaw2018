83269c6d38dd2d550b1525e5d1df650f6186fe5c
youssefgouaima <32839830+youssefgouaima@users.noreply.github.com>
2017-11-16 00:23:34 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,22 +1,31 @@
 $(document).ready(function () { //pour chargement de la page
     
+    console.log( "DOM ready!" );
+    
         $('#submit').on("click", function (event) { //ajouter un "listener" à un élément
             event.preventDefault();
             console.log( "click" );
     
-            if ($('#Nom').val() == ""|| $('#prenom').val() == "" || $('#datedenaissance').val() == "" || $('#Adresse').val() == "" || $('#Email').val() == "") {
+            if ($('#Nom').val() == ""
+             || $('#prenom').val() == "" 
+             || $('#datedenaissance').val() == "" 
+             || $('#Adresse').val() == "" 
+             || $('#Email').val() == "") {
     
-                $(".modal-title").text("Erreur");
-                $(".modal-body").html('Veuillez remplir tous les champs SVP');
-                
+      $(".modal-title").text("remplissez tous les champs");
+                $('#myModal').modal("show");
             }
             else {
-                $(".modal-body").text("Bienvenue "+$("#Nom").val());
-                $(".modal-body").html(' Votre date de naissance est : ' + $("#datedenaissance").val() + 
-            '</br>'  + 'Et vous habitez : ' + '</br>'  + ' <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#Adresse').val()+'&markers='+$('#Adresse').val()+'&size=800x400&zoom=13"/>');
+                
+                $(".modal-title").text("Bienvenue "+$("#Nom").val());
+                
+                $(".modal-body").html (' Votre date de naissance est :' +$('#datedenaissance').val() + 
+            '</br>'  + 'Et vous habitez : ' + '</br>'  + 
+            ' <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#Adresse').val()+'&markers='+$('#Adresse').val()+'&size=800x400&zoom=13&key=AIzaSyCRUEGa_filcrHmtcB_0WkQwuRW4NIcOas"/>');
+                
                 $('#myModal').modal("show");
     
             }
-    $("#myModal").modal("show");
+    
         });
     })
